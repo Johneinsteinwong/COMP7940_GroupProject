586c926cb0f3f5bf2bfade302817d56dee275cdf
Johneinsteinwong <johneinsteinwong@gmail.com>
2025-04-06 11:18:12 +0800

    Update main.py

--- main.py
+++ main.py
@@ -18,6 +18,7 @@ from telegram import Update
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
 import configparser
 import re, os, json, tweepy
+import unicodedata
 #import redis
 import psycopg2
 from datetime import datetime, timezone, timedelta
@@ -204,6 +205,7 @@ def find_faq_answer(question: str) -> str:
     global postgreConn
     cur = postgreConn.cursor()
 
+    question = unicodedata.normalize('NFKC', question)
     normed_question = re.sub(r'[^\w\s]', '', question.lower().strip())
     logging.info("Searching for normalized question: " + normed_question)
     try:
@@ -329,9 +331,10 @@ def add_faq():
             )
         """)
         for question in faq:
+            question = unicodedata.normalize('NFKC', question)
             normed_question = re.sub(r'[^\w\s]', '', question.lower().strip())
             if check_faq_exists(normed_question):
-                logging.info("FAQ already exists: " + question)
+                logging.info("FAQ already exists: " + normed_question)
                 continue
             reply = chatbot.ask(question)
             
